7a0e0a9b3ee24c33d82922a3a451d3d2a7c1f404
ArnaudPec <arnaud.pecoraro@utt.fr>
2017-12-29 18:14:35 +0100

    tbs

--- eio.c
+++ eio.c
@@ -1,22 +1,32 @@
 // gcc -o eio_file_ls eio.c `pkg-config --cflags --libs ecore eio eina` -g
+/* CPU usage cal
+ * https://stackoverflow.com/questions/16726779
+ /how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat*/
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <Eio.h>
 #include <Ecore.h>
 
-static Eina_List * processes;
+static Eina_List *processesses_list, *l;
+pthread_mutex_t mutex;
 
 typedef struct process process_t;
 struct process
 {
    int pid;
    const char * name;
-   double mem;
+   int mem;
    double proc;
 };
 
-process_t get_process_information(const char * process);
+static Eina_Bool
+get_process_information(const char * process, process_t * proc);
+
+void
+print_process(process_t * p);
 
 static Eina_Bool
 _filter_cb(void *data EINA_UNUSED, Eio_File *handler EINA_UNUSED, const char *file)
@@ -25,22 +35,56 @@ _filter_cb(void *data EINA_UNUSED, Eio_File *handler EINA_UNUSED, const char *fi
    return (last_slash[1] > '9') ? EINA_FALSE : EINA_TRUE;
 }
 
+int
+sort_cb(const void *d1, const void *d2)
+{
+   const process_t *p1 = d1;
+   const process_t *p2 = d2;
+   if(!p1) return(1);
+   if(!p2) return(-1);
+
+   return (p1->mem < p2->mem) ? 1 : -1;
+}
+
 static void
 _main_cb(void *data, Eio_File *handler EINA_UNUSED, const char *file)
 {
-   process_t p ;
+   process_t *p = NULL;
    int *number_of_listed_files = (int *)data;
 
-   p = get_process_information(file);
+   p = malloc(sizeof *p);
+
+   get_process_information(file, p);
+   pthread_mutex_lock(&mutex);
+
+   processesses_list = eina_list_append(processesses_list, p);
+   /*processesses_list = eina_list_sorted_insert(processesses_list, sort_cb, &p);*/
+   print_process(p);
+
+   pthread_mutex_unlock(&mutex);
+
+   free(p);
    (*number_of_listed_files)++;
+}
+
+void
+print_process(process_t * p)
+{
+   printf("%d %s %d %f\n", p->pid, p->name, p->mem, p->proc);
 }
 
 static void
 _done_cb(void *data, Eio_File *handler EINA_UNUSED)
 {
    int *number_of_listed_files = (int *)data;
-   printf("Number of listed files:%d\n" \
-         "ls operation is done, quitting.\n", *number_of_listed_files);
+   process_t *w;
+
+   EINA_LIST_FOREACH(processesses_list, l, w)
+       printf("%d %s %d %f\n", w->pid, w->name, w->mem, w->proc);
+
+   printf("%s %d\n", "Number files", *number_of_listed_files);
+   printf("processesses_list count: %d\n", eina_list_count(processesses_list));
+
    ecore_main_loop_quit();
 }
 
@@ -55,18 +99,13 @@ int
 main(int argc, char **argv)
 {
    int number_of_listed_files = 0;
-   process_t p = { 1, "p", 50.5, 43.3 };
-
-   printf("Playing with process %s\n", p.name);
 
    ecore_init();
    eio_init();
    eina_init();
 
-   processes = NULL;
-   /*printf("processes count: %d\n", eina_list_count(processes));*/
-   /*processes = eina_list_append(processes,  &p);*/
-   /*printf("processes count: %d\n", eina_list_count(processes));*/
+   processesses_list = NULL;
+   pthread_mutex_init(&mutex, NULL);
 
    if (argc < 2)
    {
@@ -74,52 +113,121 @@ main(int argc, char **argv)
       return  -1;
    }
    eio_file_ls(argv[1], _filter_cb, _main_cb, _done_cb, _error_cb,
-         &number_of_listed_files);
-   printf("%s\n", "hello");
-
+           &number_of_listed_files);
 
    ecore_main_loop_begin();
-   eina_list_free(processes);
+   eina_list_free(processesses_list);
    eio_shutdown();
    ecore_shutdown();
 
    return 0;
 }
 
-process_t
-get_process_information(const char * file)
+static Eina_Bool
+get_process_information(const char * file, process_t * proc)
 {
-   process_t p = { 0, NULL, 0, 0};
-   char * stat_path = NULL;
    FILE *f = NULL;
-   char buf[200];
-   int i;
+   char stat_path[100] = {0}, statm_path[100] = {0};
+   char buf[400], bufm[400], bufu[400];
+   unsigned long ttime, utime, stime, cstime, cutime, starttime, hertz;
+   double secs, uptime;
+   int i = 0;
 
-   // Get pid
-   char *last_slash = strrchr(file, '/');
-   /*printf("Processing : %s\n", ++last_slash);*/
-   p.pid = atoi(++last_slash);
-
-   // Get name
-   stat_path = (char *)malloc(sizeof(char)*(strlen(file) + strlen("/stat")));
    strcat(stat_path, file);
    strcat(stat_path, "/stat");
 
    f = fopen(stat_path, "r+");
-   if (!f) return p;
+   if (!f) {
+      return EINA_FALSE;
+   }
 
    while (fgets(buf, sizeof(buf), f) != NULL)
-   {
       buf[strlen(buf) - 1] = '\0';
-      printf("%s\n", buf);
-   }
 
    fclose(f);
 
    for (char *p = strtok(buf," "); p != NULL; p = strtok(NULL, " "))
    {
-     printf("%s\n", p);
+      switch (i++) {
+         case 0:
+            proc->pid = atoi(p);
+            break;
+         case 1:
+            proc->name = p;
+            break;
+         case 14:
+            utime = atol(p);
+            break;
+         case 15:
+            stime = atol(p);
+            break;
+         case 16:
+            cutime = atol(p);
+            break;
+         case 17:
+            cstime = atol(p);
+            break;
+         case 22:
+            starttime = atol(p);
+            break;
+      }
    }
 
-   return p;
+   strcat(statm_path, file);
+   strcat(statm_path, "/statm");
+
+   f = fopen(statm_path, "r+");
+   if (!f) {
+      return EINA_FALSE;
+   }
+
+   while (fgets(bufm, sizeof(bufm), f) != NULL)
+      bufm[strlen(bufm) - 1] = '\0';
+
+   fclose(f);
+
+   i = 0;
+   for (char *p = strtok(bufm," "); p != NULL; p = strtok(NULL, " "))
+   {
+      switch (i++) {
+         case 0:
+            proc->mem = atoi(p);
+            break;
+      }
+      break;
+   }
+
+   // CPU usage
+
+   // hertz
+   hertz = sysconf(_SC_CLK_TCK);
+   // uptime
+
+   f = fopen("/proc/uptime", "r+");
+   if (!f) {
+      return EINA_FALSE;
+   }
+
+   while (fgets(bufu, sizeof(bufu), f) != NULL)
+      bufu[strlen(bufu) - 1] = '\0';
+
+   fclose(f);
+
+   i = 0;
+   for (char *p = strtok(bufu," "); p != NULL; p = strtok(NULL, " "))
+   {
+      switch (i++) {
+         case 1:
+            {
+               uptime = atof(p);
+               break;
+            }
+      }
+   }
+
+   ttime = utime + stime + cstime + cutime;
+   secs = (double) uptime - (double) ( (double) starttime/ (double) hertz);
+   proc->proc = 100 * ( (double) ttime / (double) hertz / secs );
+
+   return EINA_TRUE;
 }
